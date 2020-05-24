6dd5ea9217290a5e594a0ce051057001ac6d3346
tomduany <tomduany@gmail.com>
2020-05-24 12:08:04 +0300

    update random

---
+++ insertTable.sql
@@ -0,0 +1,13 @@
+INSERT INTO dbo.users 
+VALUES (
+default,
+'tomy',
+'tomy1',
+'Tom',
+'Duany',
+'Isreal',
+'tomduany@gmail.com',
+'img',
+'121',
+'13'
+);
---
+++ routes/search.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var router = express.Router();
+const DButils = require("../utils/DButils");
+const utils= require("../utils/search_recipe");
+const axios = require("axios");
+
+const api_domain = "https://api.spoonacular.com/recipes";
+
+
+router.get("/search", async (req, res, next) => {
+    try {
+      const { query, cuisine, diet, intolerances, number } = req.query;
+      const search_response = await axios.get(`${api_domain}/search`, {
+        params: {
+          query: query,
+          cuisine: cuisine,
+          diet: diet,
+          intolerances: intolerances,
+          number: number,
+          apiKey: process.env.spooncular_apiKey
+        }
+      });
+      let recipes = await Promise.all(
+        search_response.data.results.map((recipe_raw) =>
+         utils.getRecipeInfo(recipe_raw.id)
+        ) 
+      );
+      recipes = recipes.map((recipe) => recipe.data);
+      //#endregion
+      const u_recipes = recipes.map((recipe) => {
+        return {
+          image: recipe.image,
+          title: recipe.title,
+          vegetarian: recipe.vegetarian,
+          vegan: recipe.vegan,
+          glutenFree: recipe.glutenFree,
+          like: recipe.aggregateLikes,
+          readyInMinutes: recipe.readyInMinutes   
+        }
+      })
+      res.send({ u_recipes });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  // function getRecipeInfo(id) {
+  //   return axios.get(`${api_domain}/${id}/information`, {
+  //     params: {
+  //       includeNutrition: false,
+  //       apiKey: process.env.spooncular_apiKey
+  //     }
+  //   });
+  // }
+
+  module.exports = router;
