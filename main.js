9787f278fd7bf7d45df85030a563b008570c9563
tomdu <46071208+tomdua@users.noreply.github.com>
2020-06-15 17:21:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -53,13 +53,6 @@ const server = app.listen(port, () => {
   console.log(`Server listen on port ${port}`);
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 app.use(cors());
 
