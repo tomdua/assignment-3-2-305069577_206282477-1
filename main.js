0790014800896c15528631009e456f61b6c1a114
tomdu <46071208+tomdua@users.noreply.github.com>
2020-06-21 17:15:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -23,13 +23,17 @@ app.use(logger("dev")); //logger
 app.use(express.json()); // parse application/json
 app.use(
   session({
-    cookieName: "session", // the cookie key name
+    cookieName: process.env.COOKIE_NAME, // the cookie key name
     secret: process.env.COOKIE_SECRET, // the encryption key
-    duration: 24 * 60 * 60 * 1000, // expired after 24 h
+    duration: 24* 60 * 60 * 1000, // expired after 20 min
     activeDuration: 0, // if expiresIn < activeDuration,
     //the session will be extended by activeDuration milliseconds
+    cookie: {
+        httpOnly: false
+    }
   })
 );
+
 
 const corsConfig = {
   origin: true,
