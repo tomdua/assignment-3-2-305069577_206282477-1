525e473e0cf326cf26a7c60fe88428f00682d234
almogsa <almogsa@post.bgu.ac.il>
2020-07-14 19:50:03 +0300

    fixing search

--- routes/search.js
+++ routes/search.js
@@ -7,25 +7,29 @@ const api_domain = "https://api.spoonacular.com/recipes";
 
 
 router.get("/search", async (req, res, next) => {
-    try {
-      const { query, cuisine, diet, intolerances, number } = req.query;
-      const search_response = await axios.get(`${api_domain}/search`, {
-        params: {
-          query: query,
-          cuisine: cuisine,
-          diet: diet,
-          intolerances: intolerances,
-          number: number,
-          apiKey: process.env.spooncular_apiKey
-        }
-      });
-      let recipes = await Promise.all(
-        search_response.data.results.map((recipe_raw) =>
-         utils.getRecipeInfo(recipe_raw.id)
-        ) 
-      );
-      recipes = recipes.map((recipe) => recipe.data);
-      const recipesP = await utils.getPrevInfo(recipes);
+  try {
+    const { query, cuisine, diet, intolerances, number } = req.query;
+    const search_response = await axios.get(`${api_domain}/search`, {
+      params: {
+        query: query,
+        cuisine: cuisine,
+        diet: diet,
+        intolerances: intolerances,
+        number: number,
+        apiKey: process.env.spooncular_apiKey
+      }
+    });
+    let recipes = await Promise.all(
+      search_response.data.results.map((recipe_raw) =>
+        utils.getRecipeInfo(recipe_raw.id)
+      )
+    );
+    recipes = recipes.map(
+      function (recipe) {
+        if(recipe.data.extendedIngredients && recipe.data.analyzedInstructions)
+      return recipe.data
+  })
+          const recipesP = await utils.getPrevInfo(recipes);
    
       res.send( recipesP );
     } catch (error) {
